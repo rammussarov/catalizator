8be4a8651151e99832bd0dc6f7832e539d9103c0
r.mussarov <r.mussarov@iopent.ru>
2020-06-09 11:50:46 +0600

    project setup

---
+++ src/test/java/dev/app/catalizator/CatalizatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.app.catalizator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalizatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
